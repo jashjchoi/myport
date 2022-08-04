4300107d6156176158983919af8a2359b18e882d
jashjchoi <jshjchoi@gmail.com>
2022-08-04 13:06:59 -0500

    fix info and updated resume

--- src/Components/contact.jsx
+++ src/Components/contact.jsx
@@ -14,7 +14,7 @@ export default function Contact() {
                     title="CONTACT"
                     subTitle="Ways To Contact Me"
                     desc="Eager to obtain a position that will expand my learning and
-                    build upon my developer skills. Should you need any further information, please do not hesitate to contact me."
+                    build upon my developer skills. Please do not hesitate to contact me."
                 />
             </div>
             <div className="container mt-45">
@@ -24,7 +24,7 @@ export default function Contact() {
                             <div className="ct-item-icon float-left ml-30 mr-20 mt-auto">
                                 <i><FaEnvelope/></i>
                             </div>
-                            <a className="btn-animation" href="mailto:jashjchoi@gmail.com">jashjchoi@gmail.com</a>
+                            <a className="btn-animation" href="mailto:jashjchoi@gmail.com">Email</a>
                         </div>
                     </div>
                     <div className="col-lg-4 col-md-6 mb-20">
@@ -32,7 +32,7 @@ export default function Contact() {
                             <div className="ct-item-icon float-left ml-30 mr-20 mt-auto">
                                 <i><FaGithub/></i>
                             </div>
-                            <a className="btn-animation" href="https://github.com/jashjchoi">jashjchoi</a>
+                            <a className="btn-animation" href="https://github.com/jashjchoi">Github</a>
                         </div>
                     </div>
                     <div className="col-lg-4 col-md-6 mb-20">
@@ -40,7 +40,7 @@ export default function Contact() {
                             <div className="ct-item-icon float-left ml-30 mr-20 mt-auto">
                                 <i><FaLinkedinIn/></i>
                             </div>
-                            <a className="btn-animation" href="https://www.linkedin.com/in/jasmine-hj-choi/">jashjchoi</a>
+                            <a className="btn-animation" href="https://www.linkedin.com/in/jasmine-hj-choi/">Linkedin</a>
                         </div>
                     </div>
                 </div>
